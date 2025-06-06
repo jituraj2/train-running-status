ac79b86a5c33bf32266a4af0bb914314e6af52a8
jituraj2 <jrajbher@gmail.com>
2025-06-06 16:25:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+from flask import Flask
 import os
 
-port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
-app.run(host="0.0.0.0", port=port)
+app = Flask(__name__)
+
+port = int(os.environ.get("PORT", 5000))
+
+@app.route("/")
+def home():
+    return "Train running status backend is live!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
